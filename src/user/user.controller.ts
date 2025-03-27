e3668bbc51abf6f78ea2ddad5ce0b63ae191045a
core2drew <calupesandro@gmail.com>
2025-03-27 10:12:13 +0800

    chore: create create user dto and use in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUser } from 'src/dto/user/create-user.dto';
+import { UserService } from 'src/services/user/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('create')
+  createUser(@Body() body: CreateUser) {
+    const { email, password } = body;
+    return this.userService.createUser(email, password);
+  }
+}
