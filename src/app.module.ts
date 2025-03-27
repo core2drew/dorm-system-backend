30229896fa13ef798539f0f53a415f12c4028587
core2drew <calupesandro@gmail.com>
2025-03-27 08:27:54 +0800

    chore: create message controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MessageController } from './message/message.controller';
+import { MessageService } from './services/message/message.service';
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MessageController],
+  providers: [AppService, MessageService],
 })
 export class AppModule {}
---
+++ src/message/message.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendMessageDto } from 'src/dto/send-message.dto';
+import { MessageService } from 'src/services/message/message.service';
+
+@Controller('message')
+export class MessageController {
+  constructor(private messageService: MessageService) {}
+
+  @Post('send-message')
+  sendMessage(@Body() body: SendMessageDto) {
+    const { phoneNumber, message } = body;
+    return this.messageService.sendMessage(phoneNumber, message);
+  }
+}
