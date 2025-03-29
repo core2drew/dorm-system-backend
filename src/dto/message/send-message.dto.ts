9169983d59a792604f8fbd6d6f64c2401288eaed
core2drew <calupesandro@gmail.com>
2025-03-29 17:12:15 +0800

    chore: update send message dto to add validation

--- src/dto/message/send-message.dto.ts
+++ src/dto/message/send-message.dto.ts
@@ -1,5 +1,11 @@
-export class SendMessageDto {
-  phoneNumber: string;
-  messagingServiceSid: string;
+import { IsArray, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class SendMessageDTO {
+  @IsArray()
+  @IsPhoneNumber()
+  phoneNumbers: string[];
+
+  @IsNotEmpty()
+  @IsString()
   message: string;
 }
