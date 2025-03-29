45b1263063ab4c35aac1e7ac08aaedf3dcc4e243
core2drew <calupesandro@gmail.com>
2025-03-29 18:31:18 +0800

    chore: remove configCache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
 import { MessageController } from './message/message.controller';
 import { MessageService } from './services/message/message.service';
 import { UserController } from './user/user.controller';
@@ -8,7 +7,7 @@ import { UserService } from './services/user/user.service';
 import { FirebaseModule } from './modules/firebase/firebase.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ cache: true }), FirebaseModule],
+  imports: [FirebaseModule],
   controllers: [AppController, MessageController, UserController],
   providers: [MessageService, UserService],
 })
--- src/modules/firebase/firebase.module.ts
+++ src/modules/firebase/firebase.module.ts
@@ -1,25 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
 import { FirebaseService } from 'src/modules/firebase/services/firebase-service/firebase-service';
 
 const firebaseProvider = {
   provide: 'FIREBASE_APP',
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService) => {
+  useFactory: () => {
     const firebaseConfig = {
       type: 'service_account',
-      project_id: configService.get<string>('FIREBASE_PROJECT_ID'),
-      private_key_id: configService.get<string>('FIREBASE_PRIVATE_KEY_ID'),
-      private_key: configService.get<string>('FIREBASE_PRIVATE_KEY'),
-      client_email: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-      client_id: configService.get<string>('FIREBASE_CLIENT_ID'),
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_uri: 'https://oauth2.googleapis.com/token',
       auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-      client_x509_cert_url: configService.get<string>(
-        'FIREBASE_CLIENT_CERT_URL',
-      ),
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
       universe_domain: 'googleapis.com',
     } as admin.ServiceAccount;
 
@@ -32,7 +28,7 @@ const firebaseProvider = {
 };
 
 @Module({
-  imports: [ConfigModule],
+  imports: [],
   providers: [firebaseProvider, FirebaseService],
   exports: [FirebaseService],
 })
