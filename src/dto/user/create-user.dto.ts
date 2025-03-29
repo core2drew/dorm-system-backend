fc444a0a98d8681e02220df7214e573916bf08f3
core2drew <calupesandro@gmail.com>
2025-03-29 14:24:01 +0800

    chore: create user DTO add validation

--- src/dto/user/create-user.dto.ts
+++ src/dto/user/create-user.dto.ts
@@ -1,4 +1,22 @@
-export class CreateUser {
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  phoneNumber: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
 }
