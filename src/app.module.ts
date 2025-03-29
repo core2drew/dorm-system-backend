f35922ce73a3c779e565d056fbf777ce181de248
core2drew <calupesandro@gmail.com>
2025-03-29 17:13:47 +0800

    chore: remove unused files and removed in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MessageController } from './message/message.controller';
 import { MessageService } from './services/message/message.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './services/user/user.service';
 import { FirebaseModule } from './modules/firebase/firebase.module';
-import { UserRepository } from './repositories/user-repository/user-repository';
 
 @Module({
   imports: [ConfigModule.forRoot({ cache: true }), FirebaseModule],
   controllers: [AppController, MessageController, UserController],
-  providers: [AppService, MessageService, UserService, UserRepository],
+  providers: [MessageService, UserService],
 })
 export class AppModule {}
