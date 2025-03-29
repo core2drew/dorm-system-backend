7bdc0775991d5378a919926345e584c86799d5be
core2drew <calupesandro@gmail.com>
2025-03-29 18:03:17 +0800

    chore: updae firebase module

--- src/modules/firebase/firebase.module.ts
+++ src/modules/firebase/firebase.module.ts
@@ -8,17 +8,19 @@ const firebaseProvider = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => {
     const firebaseConfig = {
-      type: configService.get<string>('TYPE'),
-      project_id: configService.get<string>('PROJECT_ID'),
-      private_key_id: configService.get<string>('PRIVATE_KEY_ID'),
-      private_key: configService.get<string>('PRIVATE_KEY'),
-      client_email: configService.get<string>('CLIENT_EMAIL'),
-      client_id: configService.get<string>('CLIENT_ID'),
-      auth_uri: configService.get<string>('AUTH_URI'),
-      token_uri: configService.get<string>('TOKEN_URI'),
-      auth_provider_x509_cert_url: configService.get<string>('AUTH_CERT_URL'),
-      client_x509_cert_url: configService.get<string>('CLIENT_CERT_URL'),
-      universe_domain: configService.get<string>('UNIVERSAL_DOMAIN'),
+      type: 'service_account',
+      project_id: configService.get<string>('FIREBASE_PROJECT_ID'),
+      private_key_id: configService.get<string>('FIREBASE_PRIVATE_KEY_ID'),
+      private_key: configService.get<string>('FIREBASE_PRIVATE_KEY'),
+      client_email: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+      client_id: configService.get<string>('FIREBASE_CLIENT_ID'),
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url: configService.get<string>(
+        'FIREBASE_CLIENT_CERT_URL',
+      ),
+      universe_domain: 'googleapis.com',
     } as admin.ServiceAccount;
 
     return admin.initializeApp({
