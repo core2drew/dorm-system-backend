6dec90c2f03faca8bef8dddfca46db67fa86594f
core2drew <calupesandro@gmail.com>
2025-03-29 19:06:16 +0800

    chore: use config service in message service

--- src/services/message/message.service.ts
+++ src/services/message/message.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Twilio } from 'twilio';
 import { TWILIO } from '../../enums/twilio.enum';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MessageService {
+  constructor(private configService: ConfigService) {}
   async sendMessage(phoneNumbers: string[], messageBody: string) {
-    const accountSid = process.env.SMS_SERVICE_ACC_SID;
-    const authToken = process.env.SMS_SERVICE_AUTH_TOKEN;
-    const messagingServiceSid = process.env.SMS_MESSAGING_SERVICE_ID;
+    const accountSid = this.configService.get<string>('SMS_SERVICE_ACC_SID');
+    const authToken = this.configService.get<string>('SMS_SERVICE_AUTH_TOKEN');
+    const messagingServiceSid = this.configService.get<string>(
+      'SMS_MESSAGING_SERVICE_ID',
+    );
 
     const client = new Twilio(accountSid, authToken);
     for (const number in phoneNumbers) {
