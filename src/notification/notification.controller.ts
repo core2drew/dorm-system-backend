ecbe2a4522c768af272be23f06211203da392b98
core2drew <calupesandro@gmail.com>
2025-05-10 21:07:13 +0800

    chore: update notification controller

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,15 +1,49 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { SendMessageDTO } from 'src/dto/message/send-message.dto';
+import { Controller, Post } from '@nestjs/common';
+import { BillService } from 'src/services/bill/bill.service';
+import { Timestamp } from 'firebase-admin/firestore';
 import { MessageService } from 'src/services/message/message.service';
+import { UserService } from 'src/services/user/user.service';
 
 @Controller('notification')
 export class NotificationController {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private userService: UserService,
+    private billService: BillService,
+  ) {}
+
+  private async getTenantConsumptionAndBill() {
+    const users = await this.userService.getUsers();
+    const now = Timestamp.now().toDate();
+
+    const userBill = await Promise.all(
+      users.map(async ({ id, firstName, lastName }) => {
+        const bill = await this.billService.computeTenantConsumptionAndBill(
+          id,
+          now,
+        );
+        return {
+          id,
+          firstName,
+          lastName,
+          bill,
+        };
+      }),
+    );
+    const filteredUserWithRecord = await userBill.filter(
+      (d) => !!d.bill.totalConsumption,
+    );
+    return filteredUserWithRecord;
+  }
+
+  private async generateNotifications() {
+    const userBill = await this.getTenantConsumptionAndBill();
+  }
 
   @Post('send-bill')
-  sendMessage(@Body() body: SendMessageDTO) {
-    const { userContacts, message } = body;
+  sendMessage() {
     // TODO get user account details
-    return this.messageService.sendMessage(userContacts, message);
+    const notifs = this.generateNotifications();
+    return [];
   }
 }
