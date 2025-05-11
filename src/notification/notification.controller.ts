b0003005eeffe6d3bef29f5d1d0156d4939f0496
core2drew <calupesandro@gmail.com>
2025-05-11 13:19:27 +0800

    chore: update notification controller

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,49 +1,37 @@
 import { Controller, Post } from '@nestjs/common';
 import { BillService } from 'src/services/bill/bill.service';
-import { Timestamp } from 'firebase-admin/firestore';
 import { MessageService } from 'src/services/message/message.service';
 import { UserService } from 'src/services/user/user.service';
+import { notifBillTemplate } from './notification-sms.template';
 
 @Controller('notification')
 export class NotificationController {
   constructor(
     private messageService: MessageService,
-    private userService: UserService,
     private billService: BillService,
+    private userService: UserService,
   ) {}
 
-  private async getTenantConsumptionAndBill() {
-    const users = await this.userService.getUsers();
-    const now = Timestamp.now().toDate();
-
-    const userBill = await Promise.all(
-      users.map(async ({ id, firstName, lastName }) => {
-        const bill = await this.billService.computeTenantConsumptionAndBill(
-          id,
-          now,
-        );
-        return {
-          id,
-          firstName,
-          lastName,
-          bill,
-        };
-      }),
-    );
-    const filteredUserWithRecord = await userBill.filter(
-      (d) => !!d.bill.totalConsumption,
-    );
-    return filteredUserWithRecord;
-  }
-
   private async generateNotifications() {
-    const userBill = await this.getTenantConsumptionAndBill();
+    const users = await this.userService.getUsers();
+    const userBills =
+      await this.billService.getTenantsConsumptionAndBill(users);
+
+    const notifs = userBills.map((userBill) => {
+      const { mobileNo } = userBill;
+      return {
+        message: notifBillTemplate(userBill),
+        mobileNo,
+      };
+    });
+
+    return notifs;
   }
 
   @Post('send-bill')
   sendMessage() {
     // TODO get user account details
-    const notifs = this.generateNotifications();
-    return [];
+
+    return this.generateNotifications();
   }
 }
