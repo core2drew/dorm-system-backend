b949238d0537f599fe66ee44369caa8d4b1ec6ed
core2drew <calupesandro@gmail.com>
2025-05-13 23:04:52 +0800

    refactor:  update message service

--- src/services/message/message.service.ts
+++ src/services/message/message.service.ts
@@ -2,64 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { Twilio } from 'twilio';
 import { TWILIO } from '../../enums/twilio.enum';
 import { ConfigService } from '@nestjs/config';
-import { FirebaseService } from 'src/modules/firebase/services/firebase-service/firebase-service';
-import { Timestamp } from 'firebase-admin/firestore';
 import { UserContactDTO } from 'src/dto/message/send-message.dto';
 import { handleServiceError } from 'src/shared/utils/error-handler.util';
 @Injectable()
 export class MessageService {
   readonly serviceName = 'message-service';
-  constructor(
-    private configService: ConfigService,
-    private firebase: FirebaseService,
-  ) {}
+  constructor(private configService: ConfigService) {}
 
-  async sendMessage(userContact: UserContactDTO[], messageBody: string) {
+  async sendMessage(contact: UserContactDTO, messageBody: string) {
     const accountSid = this.configService.get<string>('SMS_SERVICE_ACC_SID');
     const authToken = this.configService.get<string>('SMS_SERVICE_AUTH_TOKEN');
     const messagingServiceSid = this.configService.get<string>(
       'SMS_MESSAGING_SERVICE_ID',
     );
-    const result = [];
-    const sendToUids = [];
     const client = new Twilio(accountSid, authToken);
-    let messagesRef: FirebaseFirestore.DocumentReference<
-      FirebaseFirestore.DocumentData,
-      FirebaseFirestore.DocumentData
-    >;
 
     try {
-      for (const contact of userContact) {
-        const { uid, mobileNo } = contact;
-        const response = await client.messages.create({
-          body: messageBody,
-          from: TWILIO.SENDER_NO,
-          messagingServiceSid,
-          to: mobileNo,
-        });
-        sendToUids.push(uid);
-        result.push({
-          status: 'success',
-          mobileNo,
-          id: response.sid,
-        });
-      }
-    } catch (error) {
-      handleServiceError(error, `${this.serviceName}`);
-    } finally {
-      messagesRef = await this.firebase.initCollection('messages').add({
-        message: messageBody,
-        timestamp: Timestamp.now(),
-        uids: sendToUids,
+      const { mobileNo } = contact;
+      const response = await client.messages.create({
+        body: messageBody,
+        from: TWILIO.SENDER_NO,
+        messagingServiceSid,
+        to: mobileNo,
       });
+      return response.sid;
+    } catch (error) {
+      handleServiceError(error, `${this.serviceName}-sendMessage`);
     }
-
-    const messageData = await messagesRef.get();
-
-    return {
-      ...messageData.data(),
-      timestamp: messageData.createTime.toDate(),
-      id: messagesRef.id,
-    };
   }
 }
