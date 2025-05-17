7341827003274e12724cf4889b504e25c611b476
core2drew <calupesandro@gmail.com>
2025-05-17 18:36:31 +0800

    chore: update message service

--- src/services/message/message.service.ts
+++ src/services/message/message.service.ts
@@ -1,31 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { Twilio } from 'twilio';
-import { TWILIO } from '../../enums/twilio.enum';
 import { ConfigService } from '@nestjs/config';
 import { UserContactDTO } from 'src/dto/message/send-message.dto';
 import { handleServiceError } from 'src/shared/utils/error-handler.util';
+import { Vonage } from '@vonage/server-sdk';
+import { AuthInterface } from '@vonage/auth';
+import { randomUUID } from 'crypto';
 @Injectable()
 export class MessageService {
   readonly serviceName = 'message-service';
   constructor(private configService: ConfigService) {}
 
   async sendMessage(contact: UserContactDTO, messageBody: string) {
-    const accountSid = this.configService.get<string>('SMS_SERVICE_ACC_SID');
-    const authToken = this.configService.get<string>('SMS_SERVICE_AUTH_TOKEN');
-    const messagingServiceSid = this.configService.get<string>(
-      'SMS_MESSAGING_SERVICE_ID',
-    );
-    const client = new Twilio(accountSid, authToken);
+    const apiKey = this.configService.get<string>('VONAGE_API_KEY');
+    const apiSecret = this.configService.get<string>('VONAGE_API_SECRET');
+
+    const vonage = new Vonage({
+      apiKey,
+      apiSecret,
+    } as AuthInterface);
 
     try {
+      const from = 'Dorm flow';
       const { mobileNo } = contact;
-      const response = await client.messages.create({
-        body: messageBody,
-        from: TWILIO.SENDER_NO,
-        messagingServiceSid,
-        to: mobileNo,
-      });
-      return response.sid;
+      await vonage.sms
+        .send({ to: mobileNo, from, text: messageBody })
+        .then((resp) => {
+          console.log('Message sent successfully');
+          console.log(resp);
+        })
+        .catch((err) => {
+          console.log('There was an error sending the messages.');
+          console.error(err);
+        });
+      return randomUUID();
     } catch (error) {
       handleServiceError(error, `${this.serviceName}-sendMessage`);
     }
