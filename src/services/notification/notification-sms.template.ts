6c79d28464695dc58ed5e5b0ba51c033519065ba
core2drew <calupesandro@gmail.com>
2025-05-17 19:17:58 +0800

    chore: update template

--- src/services/notification/notification-sms.template.ts
+++ src/services/notification/notification-sms.template.ts
@@ -4,11 +4,5 @@ export const notifBillTemplate = (data: TenantConsumptionBill) => {
   const { user, bill } = data;
   const { firstName, lastName } = user;
   const { totalConsumption, totalPricePerMeter, totalBill } = bill;
-  return `
-        Hi ${firstName} ${lastName}, your water bill is ready.
-        Total Consumption: ${totalConsumption} m³.
-        Average Price: ₱${totalPricePerMeter} m³.
-        Total Bill: ₱${totalBill}
-        Thank you!
-    `;
+  return `Hi ${firstName} ${lastName}, your water bill is ready. Total Consumption: ${totalConsumption} m³. Average Price: ₱${totalPricePerMeter} m³. Total Bill: ₱${totalBill} Thank you!`;
 };
